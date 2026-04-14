5f16181a835b621db7bf40ea1f5ea26195ea4d25
Matheus Fellipe <mfellipedev@gmail.com>
2026-04-13 22:15:06 -0300

    feat: implement method remove

--- src/main/java/warehouse/service/InventoryManager.java
+++ src/main/java/warehouse/service/InventoryManager.java
@@ -29,7 +29,31 @@ public class InventoryManager {
         }
     }
 
-    public Inventory getInventory(String id){
+    public Inventory getInventory(String id) {
         return this.inventory.get(id);
     }
+
+    public int remove(String id, Product product, int quantity) {
+        Address address = this.warehouse.getAddress(id);
+        if (product == null) {
+            throw new IllegalArgumentException("Verifique se o endereço ou produto estão corretos");
+        }
+        Inventory inventory1 = getInventory(address.getId());
+        if (inventory1 == null) {
+            throw new IllegalArgumentException("Não existe um produto armazenado no endereço");
+        }
+        if (quantity <= 0 || inventory1.getQuantity() < quantity) {
+            throw new IllegalArgumentException("Quantidade invalida para remoção");
+        } else if (!inventory1.getProduct().equals(product)) {
+            throw new IllegalArgumentException("O produto não pode ser diferente do armazenado");
+        }
+        int status = inventory1.updatingBalance(quantity);
+        if (status == 0) {
+            this.inventory.remove(address.getId());
+        }
+        return status;
+
+    }
+
+
 }
