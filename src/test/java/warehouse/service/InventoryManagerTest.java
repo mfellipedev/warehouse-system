b84ec869971514507d6f06153db3279d0e2a3572
Matheus Fellipe <mfellipedev@gmail.com>
2026-04-13 22:17:35 -0300

    test: create tests for method remove

--- src/test/java/warehouse/service/InventoryManagerTest.java
+++ src/test/java/warehouse/service/InventoryManagerTest.java
@@ -2,20 +2,28 @@ package warehouse.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import warehouse.domain.Product;
-import warehouse.domain.Warehouse;
+import warehouse.domain.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class InventoryManagerTest {
     private Warehouse warehouse;
     private InventoryManager inventoryManager;
+    private Street street;
+    private Level level;
+    private Column column;
+    private Address address;
+    private Product product;
 
     @BeforeEach
     void setup() {
         warehouse = new Warehouse(26, 3, 8);
         inventoryManager = new InventoryManager(warehouse);
+        street = new Street("A");
+        level = new Level(3);
+        column = new Column(10);
+        address = new Address(street, column, level);
+        product = new Product("Camisa", "2023402342");
     }
 
     @Test
@@ -34,5 +42,46 @@ public class InventoryManagerTest {
         inventoryManager.store("A-1-1", new Product("Camisa", "202400332"), 10);
         assertThrows(IllegalArgumentException.class, () -> inventoryManager.store("A-1-1", new Product("Moletom", "20229099"), 20));
     }
+
+    @Test
+    void shouldNotRemoveWhenAddressIsNull() {
+        assertThrows(IllegalArgumentException.class, () -> inventoryManager.remove(null, product, 20));
+    }
+
+    @Test
+    void shouldNotRemoveWhenProductIsNull() {
+        product = null;
+        assertThrows(IllegalArgumentException.class, () -> inventoryManager.remove("A-1-1", product, 20));
+    }
+
+    @Test
+    void shouldNotRemoveWhenQuantityIsGreatherThanBalance() {
+        Product productTest = new Product("Cinto", "202903223");
+        inventoryManager.store("A-1-1", productTest, 20);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> inventoryManager.remove("A-1-1", productTest, 21));
+        assertEquals("Quantidade invalida para remoção", exception.getMessage());
+    }
+
+    @Test
+    void shouldNotRemoveWhenProductIsDifferent() {
+        Product productTest = new Product("Cinto", "202903223");
+        Product productDifferent = new Product("Meia", "202912901");
+        inventoryManager.store("A-1-1", productTest, 20);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> inventoryManager.remove("A-1-1", productDifferent, 20));
+        assertEquals("O produto não pode ser diferente do armazenado",exception.getMessage());
+    }
+    @Test
+    void shouldUpdatingBalance(){
+        Product productTest = new Product("Cinto", "202903223");
+        inventoryManager.store("A-1-1", productTest, 20);
+        inventoryManager.remove("A-1-1",productTest,19);
+        assertEquals(1,this.inventoryManager.getInventory("A-1-1").getQuantity());
+    }
+    @Test
+    void shouldDeleteInventoryWhenBalanceEqualsToZero(){
+        Product productTest = new Product("Cinto", "202903223");
+        inventoryManager.store("A-1-1", productTest, 20);
+        inventoryManager.remove("A-1-1",productTest,20);
+        assertNull(this.inventoryManager.getInventory("A-1-1"));
+    }
 }
-//
