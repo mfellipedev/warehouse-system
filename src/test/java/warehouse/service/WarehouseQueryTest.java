5ad0c6d0b5d84ee65127123be11e7e6bce13e795
Matheus Fellipe <mfellipedev@gmail.com>
2026-04-20 21:35:51 -0300

    test: create WarehouseQueryTest class

--- src/test/java/warehouse/service/WarehouseQueryTest.java
+++ src/test/java/warehouse/service/WarehouseQueryTest.java
@@ -2,6 +2,7 @@ package warehouse.service;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import warehouse.domain.Product;
 import warehouse.domain.Warehouse;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,7 +20,28 @@ public class WarehouseQueryTest {
     }
 
     @Test
-    void shouldReturnListSizeEqualsToZeroWhenProductIsNull(){
-        assertEquals(0, warehouseQuery.queryProductsInStock(null).size());
+    void shouldReturnListSizeEqualsToZeroWhenProductNotExistOrIsNulll(){
+        assertEquals(0, warehouseQuery.queryProductInStock(null).size());
     }
+   @Test
+    void shoulReturnListSizeEqualsToOneWhenQueryIsValid(){
+       Product productTest = new Product("Moletom","202932032");
+       this.inventoryManager.store("A-1-1",productTest,20);
+       assertEquals(1,this.warehouseQuery.queryProductInStock("202932032").size());
+       assertEquals("ID: A-1-1 | Quantidade: 20",this.warehouseQuery.queryProductInStock("202932032").get(0));
+   }
+
+@Test
+    void shouldReturnAllAddresses(){
+        assertEquals(780,this.warehouseQuery.addressesQuery().size());
+}
+@Test
+    void shouldReturnCorrectOutput(){
+        assertEquals("A-1-1",this.warehouseQuery.addressesQuery().get(0).getId());
+}
+
+@Test
+    void shouldReturnCorrectOutputOfStreets(){
+        assertEquals("A",this.warehouseQuery.streetsQuery().get(0).street());
+}
 }
