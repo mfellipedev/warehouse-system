4d972bc1d484853bf65c3f6cd630fe56c867538c
Matheus Fellipe <mfellipedev@gmail.com>
2026-04-23 22:08:08 -0300

    feat: implement read method for numeric input validation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,27 +13,41 @@ public class Main {
 
         CLI.welcomeMessage();
 
-        System.out.println("Informe a quantidade de ruas no armazém: (Somente valores entre 1 e 26)");
-        int quantityStreets = Integer.parseInt(scanner.nextLine());
-        System.out.println("Informe a quantidade de níveis: (Somente valores entre 1 e 3)");
-        int quantityLevels = Integer.parseInt(scanner.nextLine());
-        System.out.println("Informe a quantidade de colunas: (Somente valores entre 1 e 10)");
-        int quantityColumns = Integer.parseInt(scanner.nextLine());
+            int quantityStreets = read(scanner, "Informe a quantidade de ruas no armazém: (Somente valores entre 1 e 26)", 1, 26);
+            int quantityLevels = read(scanner, "Informe a quantidade de níveis: (Somente valores entre 1 e 3)", 1, 3);
+            int quantityColumns = read(scanner, "Informe a quantidade de colunas: (Somente valores entre 1 e 10)", 1, 10);
 
 
-        try {
             Warehouse warehouse = new Warehouse(quantityStreets, quantityLevels, quantityColumns);
             InventoryManager inventoryManager = new InventoryManager(warehouse);
             WarehouseQuery warehouseQuery = new WarehouseQuery(inventoryManager);
             CLI cli = new CLI(inventoryManager, warehouseQuery);
 
-            cli.menu();
             cli.start();
 
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+
+
+    }
+
+    public static int read(Scanner scanner, String message, int min, int max) {
+        while (true) {
+            try {
+                System.out.println(message);
+                int value = Integer.valueOf(scanner.nextLine());
+
+                if (value < min || value > max) {
+                    throw new IllegalArgumentException("Verifique se os valores atendem as regras!");
+                }
+
+                return value;
+
+            } catch (IllegalArgumentException e) {
+                if (e instanceof NumberFormatException) {
+                    System.out.println("Somente números validos são permitidos!");
+                } else {
+                    System.out.println(e.getMessage());
+                }
+            }
         }
-
-
     }
 }
