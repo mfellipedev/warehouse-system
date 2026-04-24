5bc3709610954dcdac533541b68ab1203a384beb
Matheus Fellipe <mfellipedev@gmail.com>
2026-04-23 22:11:51 -0300

    feat: update Address toString for better readability

--- src/main/java/warehouse/domain/Address.java
+++ src/main/java/warehouse/domain/Address.java
@@ -10,19 +10,28 @@ public class Address {
 
     public Address(Street street, Column column, Level level) {
 
-        this.street = Objects.requireNonNull(street,"A rua não pode ser null");
-        this.column = Objects.requireNonNull(column,"A coluna não pode ser null");
-        this.level = Objects.requireNonNull(level,"O nível não pode ser null");
+        this.street = Objects.requireNonNull(street, "A rua não pode ser null");
+        this.column = Objects.requireNonNull(column, "A coluna não pode ser null");
+        this.level = Objects.requireNonNull(level, "O nível não pode ser null");
         this.id = generateID();
     }
 
     private String generateID() {
-        return street.street() + "-" + column.getColumn() + "-" + level.level();
+        return street.street() + "-" + level.level() + "-" + column.getColumn();
     }
 
     public String getId() {
         return id;
     }
+
+    public int getLevel() {
+        return level.level();
+    }
+
+    public Column getColumn() {
+        return column;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -34,5 +43,10 @@ public class Address {
     @Override
     public int hashCode() {
         return Objects.hash(street, column, level, id);
+    }
+
+    @Override
+    public String toString() {
+        return "ID Endereço: " + this.id + "| Rua: " + this.street.street() + "| Nível: " + this.level.level() + "| Coluna: " + this.column.getColumn() + "\n";
     }
 }
