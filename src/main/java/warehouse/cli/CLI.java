dc6557627250c8028a3401dcf17451698b5062e9
MatheusDev <mfellipedev@gmail.com>
2026-05-05 18:41:42 -0300

    feat: replace 'Street Report' with 'Transaction History'

--- src/main/java/warehouse/cli/CLI.java
+++ src/main/java/warehouse/cli/CLI.java
@@ -1,9 +1,13 @@
 package warehouse.cli;
 
 import warehouse.domain.Product;
+import warehouse.domain.TransactionLog;
 import warehouse.service.InventoryManager;
+import warehouse.service.LogManager;
+import warehouse.service.LogQuery;
 import warehouse.service.WarehouseQuery;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -11,11 +15,13 @@ public class CLI {
     private final InventoryManager inventoryManager;
     private final WarehouseQuery warehouseQuery;
     private final Scanner scanner;
+    private LogQuery logQuery;
 
     public CLI(InventoryManager inventoryManager, WarehouseQuery warehouseQuery) {
         this.inventoryManager = inventoryManager;
         this.warehouseQuery = warehouseQuery;
         this.scanner = new Scanner(System.in);
+        this.logQuery = new LogQuery(inventoryManager.returnLogManager());
     }
 
     public static void welcomeMessage() {
@@ -43,7 +49,7 @@ public class CLI {
         System.out.println("║3. Remover Saldo                  ║");
         System.out.println("║4. Buscar Produto                 ║");
         System.out.println("║5. Relatório De Endereços         ║");
-        System.out.println("║6. Relatório De Ruas              ║");
+        System.out.println("║6. Histórico De Transação         ║");
         System.out.println("║0. Sair                           ║");
         System.out.println("╚══════════════════════════════════╝");
     }
@@ -77,7 +83,7 @@ public class CLI {
                     relatorioEnderecos();
                     continue;
                 case 6:
-                    relatorioRuas();
+                    logs();
                     continue;
             }
         }
@@ -145,7 +151,7 @@ public class CLI {
 
         try {
             System.out.println("Saldo Restante: " + inventoryManager.remove(id, sku, quantity) + "\n" +
-            "Saldo Removido: " + quantity);
+                    "Saldo Removido: " + quantity);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
@@ -165,15 +171,54 @@ public class CLI {
     public void relatorioEnderecos() {
         System.out.println("Todos os endereços serão exibidos!");
         System.out.println("╠══════════════════════════════════════════════════════════════════════════════════════════╣");
-        Map<String,String> addresses = warehouseQuery.completeAdressesQuery();
-        addresses.forEach((id,status) -> {
+        Map<String, String> addresses = warehouseQuery.completeAddressesQuery();
+        addresses.forEach((id, status) -> {
             System.out.printf("║ Endereço: %-12s ║ Status: %-55s ║%n", id, status);
         });
     }
 
-    public void relatorioRuas(){
-        System.out.println("Ruas Disponíveis: ");
-        warehouseQuery.streetsQuery().stream()
-                .forEach(System.out::println);
+    public void logs() {
+        System.out.println("1. Exibir Histórico Geral");
+        System.out.println("2. Exibir Histórico por Produto");
+
+        String option = scanner.nextLine();
+
+        if (option.equals("1")) {
+            List<TransactionLog> listLogs = logQuery.getTransactionLogs();
+            if (listLogs.isEmpty()) {
+                System.out.println("Nenhum registro encontrado!");
+            } else {
+                imprimirCabecalho();
+                listLogs.forEach(System.out::println);
+                imprimirRodape();
+            }
+        } else if (option.equals("2")) {
+            System.out.println("Qual produto deseja exibir?");
+            String product = scanner.nextLine();
+            List<TransactionLog> listLogs = logQuery.getLogsByProduct(product);
+            if (listLogs.isEmpty()) {
+                System.out.println("Nenhum registro encontrado!");
+            } else {
+                imprimirCabecalho();
+                listLogs.forEach(System.out::println);
+                imprimirRodape();
+            }
+
+        } else {
+            System.out.println("Informe uma opção valida!");
+        }
+    }
+
+    private void imprimirCabecalho() {
+        String headerFormat = "| %-19s | %-22s | %-10s | %-15s |%-8s | %-5s";
+        int larguraTotal = 95;
+
+        System.out.println("\n" + "=".repeat(larguraTotal));
+        System.out.printf(headerFormat + "%n", "DATA/HORA", "TIPO", "SKU", "PRODUTO", "LOCAL", "QTD");
+        System.out.println("-".repeat(larguraTotal));
+    }
+
+    private void imprimirRodape() {
+        System.out.println("=".repeat(95));
     }
 }
